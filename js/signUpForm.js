d87984d4c0c0b39e90f2b2ffa81dcee744da96f3
AliNough <112473635+AliNough@users.noreply.github.com>
2023-03-26 22:49:34 +0200

    sign-up-form:0.5

--- js/signUpForm.js
+++ js/signUpForm.js
@@ -7,6 +7,10 @@ const passEL = document.querySelector("#js-password");
 const passRptEL = document.querySelector("#js-password-rpt");
 const cardNrEl = document.querySelector("#js-cardnumber");
 const cvcEl = document.querySelector("#js-cvc");
+const submitEl = document.querySelector("#js-submit-form");
+const formEl = document.querySelector("#js-form");
+const successEl = document.querySelector("#js-success-holder");
+const mopGrpHolder = document.querySelector("#js-mob-grp");
 
 fnameEl.addEventListener("blur", (e) => {
   const fname = e.target.value.trim();
@@ -27,8 +31,8 @@ fnameEl.addEventListener("blur", (e) => {
     e.target.classList.remove("is-valid");
     e.target.classList.add("is-error");
     validationEle.innerHTML = "• Please enter you full name";
-    validationEle.classList.add("show-msg");
-    validationEle.classList.remove("hide-msg");
+    validationEle.classList.add("show");
+    validationEle.classList.remove("hide");
   }
 });
 
@@ -41,15 +45,15 @@ lnameEl.addEventListener("blur", (e) => {
     e.target.classList.add("is-valid");
     e.target.classList.remove("is-error");
 
-    validationEle.classList.remove("show-msg");
-    validationEle.classList.add("hide-msg");
+    validationEle.classList.remove("show");
+    validationEle.classList.add("hide");
   } else {
     e.target.classList.remove("is-valid");
     e.target.classList.add("is-error");
 
     validationEle.innerHTML = "• Please enter you full name";
-    validationEle.classList.add("show-msg");
-    validationEle.classList.remove("hide-msg");
+    validationEle.classList.add("show");
+    validationEle.classList.remove("hide");
   }
 });
 
@@ -62,15 +66,15 @@ emailEl.addEventListener("blur", (e) => {
   if (emailRegex.test(email)) {
     e.target.classList.add("is-valid");
     e.target.classList.remove("is-error");
-    validationEle.classList.remove("show-msg");
-    validationEle.classList.add("hide-msg");
+    validationEle.classList.remove("show");
+    validationEle.classList.add("hide");
   } else {
     e.target.classList.remove("is-valid");
     e.target.classList.add("is-error");
 
     validationEle.innerHTML = "• Invalid email. Example: john@example.com";
-    validationEle.classList.add("show-msg");
-    validationEle.classList.remove("hide-msg");
+    validationEle.classList.add("show");
+    validationEle.classList.remove("hide");
   }
 });
 phnNrEl.addEventListener("blur", (e) => {
@@ -81,15 +85,15 @@ phnNrEl.addEventListener("blur", (e) => {
   if (nrOnlyRegex.test(phoneNr)) {
     e.target.classList.add("is-valid");
     e.target.classList.remove("is-error");
-    validationEle.classList.remove("show-msg");
-    validationEle.classList.add("hide-msg");
+    validationEle.classList.remove("show");
+    validationEle.classList.add("hide");
   } else {
     e.target.classList.remove("is-valid");
     e.target.classList.add("is-error");
 
     validationEle.innerHTML = "• Invalid phone number";
-    validationEle.classList.add("show-msg");
-    validationEle.classList.remove("hide-msg");
+    validationEle.classList.add("show");
+    validationEle.classList.remove("hide");
   }
 });
 passEL.addEventListener("blur", (e) => {
@@ -100,52 +104,79 @@ passEL.addEventListener("blur", (e) => {
   if (passRegex.test(pass)) {
     e.target.classList.add("is-valid");
     e.target.classList.remove("is-error");
-    validationEle.classList.remove("show-msg");
-    validationEle.classList.add("hide-msg");
+    validationEle.classList.remove("show");
+    validationEle.classList.add("hide");
   } else {
     e.target.classList.remove("is-valid");
     e.target.classList.add("is-error");
 
     validationEle.innerHTML =
-      "• password must have atleast 8 characters long. and a capital letter letter";
-    validationEle.classList.add("show-msg");
-    validationEle.classList.remove("hide-msg");
+      "• Password must have atleast 8 characters. and 1 capital letter";
+    validationEle.classList.add("show");
+    validationEle.classList.remove("hide");
   }
 });
 
-passRptEL.addEventListener("blur", (e) => {
-  if (this.value.trim() === passEL.value.trim()) {
-    e.target.classList.add("is-valid");
-    e.target.classList.remove("is-error");
-    validationEle.classList.remove("show-msg");
-    validationEle.classList.add("hide-msg");
-  } else {
-    e.target.classList.remove("is-valid");
-    e.target.classList.add("is-error");
+// passRptEL.addEventListener("blur", (e) => {
 
-    validationEle.innerHTML = "• Password does not match";
-    validationEle.classList.add("show-msg");
-    validationEle.classList.remove("hide-msg");
-  }
-});
+//   if (this.value.trim() === passEL.value.trim()) {
+//     e.target.classList.add("is-valid");
+//     e.target.classList.remove("is-error");
+//     validationEle.classList.remove("show");
+//     validationEle.classList.add("hide");
+//   } else {
+//     e.target.classList.remove("is-valid");
+//     e.target.classList.add("is-error");
+
+//     validationEle.innerHTML = "• Password does not match";
+//     validationEle.classList.add("show");
+//     validationEle.classList.remove("hide");
+//   }
+// });
 
 cardNrEl.addEventListener("blur", (e) => {
-  const cardNr = this.value.trim();
+  const cardNr = e.target.value.trim();
   const cardRegEx = /\b\d{4}([- ])?\d{4}\1\d{4}\1\d{4}\b/;
+  const validationEle = mopGrpHolder.nextElementSibling;
 
   if (cardRegEx.test(cardNr)) {
     e.target.classList.add("is-valid");
     e.target.classList.remove("is-error");
-    validationEle.classList.remove("show-msg");
-    validationEle.classList.add("hide-msg");
+    validationEle.classList.remove("show");
+    validationEle.classList.add("hide");
   } else {
     e.target.classList.remove("is-valid");
     e.target.classList.add("is-error");
 
     validationEle.innerHTML =
-      "• password must have atleast 8 characters long. and a capital letter letter";
-    validationEle.classList.add("show-msg");
-    validationEle.classList.remove("hide-msg");
+      "• Invalid card number. Example: 1234 1324 1234 1234";
+    validationEle.classList.add("show");
+    validationEle.classList.remove("hide");
   }
 });
-cvcEl.addEventListener("blur", (e) => {});
+cvcEl.addEventListener("blur", (e) => {
+  const cvc = e.target.value.trim();
+  const cvcRegex = /\b\d{3,4}\b/;
+  const validationEle = mopGrpHolder.nextElementSibling;
+
+  if (cvcRegex.test(cvc)) {
+    e.target.classList.add("is-valid");
+    e.target.classList.remove("is-error");
+    validationEle.classList.remove("show");
+    validationEle.classList.add("hide");
+  } else {
+    e.target.classList.remove("is-valid");
+    e.target.classList.add("is-error");
+
+    validationEle.innerHTML = "• invalid cvc number. example: 123";
+    validationEle.classList.add("show");
+    validationEle.classList.remove("hide");
+  }
+});
+
+submitEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("hellooooo");
+  formEl.classList.add("hide");
+  successEl.classList.remove("hide");
+});
